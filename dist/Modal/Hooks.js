29629c6ea26163021f13458b4d861fc348db7564
Dylan Pellé <dylan.pelle.dev@gmail.com>
2022-11-05 12:04:35 +0100

    Compile lib with babel

---
+++ dist/Modal/Hooks.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+const useModal = () => {
+  const [openedModals, setModals] = useState({});
+  const toggleModal = id => {
+    setModals({
+      ...openedModals,
+      [id]: !openedModals[id]
+    });
+  };
+  return {
+    openedModals,
+    toggleModal
+  };
+};
+export default useModal;
---
+++ dist/index.js
@@ -0,0 +1,3 @@
+import Modal, { useModal } from "./Modal/Modal";
+export { useModal };
+export default Modal;
